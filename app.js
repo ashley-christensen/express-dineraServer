bca9ccb86ceb3077655362ab2f5262067e582c34
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-09-06 17:44:58 -0700

    deploy2

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
 
-// const url = 'mongodb://localhost:27017/dinera';
-const uri = process.env.DB_URI;
-const connect = mongoose.connect(uri, {
-  dbName: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const url = 'mongodb://localhost:27017/dinera';
+// const uri = process.env.DB_URI;
+const connect = mongoose.connect(url, {
+  // dbName: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // pass: process.env.DB_PASS,
+  useCreateIndex: true,
   useFindAndModify: false,
-  useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 connect.then(() => {
   console.log('Connection estabislished with MongoDB');
@@ -38,11 +38,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //middleware
-app.use(cors({
-  // react app location -- for testing on localhost - http://localhost:3000
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+// app.use(cors({
+//   // react app location -- for testing on localhost - http://localhost:3000
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,30 +50,32 @@ app.use(cookieParser());
 
 //authenticatation Auth()
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');//basic challenge to client
-    err.status = 401; //standard err when credentials not provided
-    return next(err);
-  }
-  //Client response to challenge comes back with authheader
-  //parse user/pass into array ['user', 'pass]
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {//granted: pass control to next middleware
-    return next();//access granted
-  } else {
-    const err = new Error('You are not authenticated');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');//basic challenge to client
+      err.status = 401; //standard err when credentials not provided
+      return next(err);
+    }
+
+    //parse user/pass into array ['user', 'pass]
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {//granted: pass control to next middleware
+      res.cookie('user', 'admin', { signed: true });
+      return next();//access granted
+    } else {
+      const err = new Error('You are not authenticated');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
-app.use(auth);
+// app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
